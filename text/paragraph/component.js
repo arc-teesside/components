dbcbcca64f8aad3602eb2c85e0f83db18f7c8fa7
Franck <franck.o.perrin@free.fr>
2014-06-25 18:23:38 +0100

    + font-size for the paragraph
    Renamed "Configurable placeholder panel" to "Placeholder"
    Fixed background image type for placeholder

--- text/paragraph/component.js
+++ text/paragraph/component.js
@@ -3,6 +3,7 @@ var paragraph = function (ID) {
 	
 	this.id = ID;
 	this.text = "";
+	this.fontSize = null;
 	// further attributes here (attributes in cdlmanifest.json)
 	
 	var _this = this;
@@ -10,6 +11,9 @@ var paragraph = function (ID) {
 	this.init = function() {
 	    var ID = "#" + _this.id;
         $(ID+"-text").text(_this.text);
+		if(_this.fontSize) {
+			$(ID+"-text").css("font-size", _this.fontSize+"px");
+		}
 	}
 // Further functions here (actions in cdlmanifest.json)
 
